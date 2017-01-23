50644ff0401cc50d5d9fdc7d0f369dbb8172bc4c
Zenon Trujillo <zenon.t@outlook.com>
2017-01-22 20:48:07 -0800

    query string unit tests

---
+++ HttpClientExtended/HttpClientExtended.Common.Test/QueryStringTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HttpClientExtended.Common.Test
+{
+    public class QueryStringTest
+    {
+        [Theory]
+        [InlineData("someKey", true, "True")]
+        [InlineData("someKey", 10, "10")]
+        [InlineData("someKey", 10.103, "10.103")]
+        [InlineData("someKey", null, "")]
+        public void ShouldAddAndParseMultipleDataTypes(string key, object value, string expected)
+        {
+            // act
+            QueryString queryString = new QueryString();
+            queryString.AddQueryString(key, value);
+
+            // assert
+            Assert.True(queryString.Single().Value == expected);
+        }
+
+        [Theory]
+        [InlineData("someKey", new int[] { 1, 2, 3, 4 }, new string[] { "1", "2", "3", "4" })]
+        public void ShouldAddAndParseArrays(string key, object value, string[] expected)
+        {
+            // act
+            QueryString queryString = new QueryString();
+            queryString.AddQueryString(key, value);
+            KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
+
+
+            // assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.True(parsedValue[i].Value == expected[i]);
+            }
+        }
+
+        [Fact]
+        public void ShouldAddAndParseIEnumerableFromList()
+        {
+            // act
+            const string key = "someKey";
+            List<int> value = new List<int> { 1, 2, 3, 4 };
+            var expected = new string[] { "1", "2", "3", "4" };
+
+            // act
+            QueryString queryString = new QueryString();
+            queryString.AddQueryString(key, value);
+            KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
+
+            // assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.True(parsedValue[i].Value == expected[i]);
+            }
+        }
+    }
+}
--- HttpClientExtended/src/HttpClientExtended.Common/QueryString.cs
+++ HttpClientExtended/src/HttpClientExtended.Common/QueryString.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace HttpClientExtended.Common
@@ -17,22 +18,15 @@ namespace HttpClientExtended.Common
             return Convert.ToString(value);
         }
 
-        protected virtual string TrimKeyOrThrow(string key)
+        public virtual void AddKeyValuePair(string key, string value)
         {
-            if (string.IsNullOrEmpty(key)) throw new ArgumentException("The key cannot be empty or blank", "key");
-            return key.Trim();
-        }
-
-        public void AddKeyValuePair(string key, string value)
-        {
-            key = TrimKeyOrThrow(key);
+            if (string.IsNullOrEmpty(key))  throw new ArgumentNullException("key");
             if (string.IsNullOrEmpty(value)) return;
 
-            value = value?.Trim();
-            Add(new KeyValuePair<string, string>(key, value));
+            Add(new KeyValuePair<string, string>(key.Trim(), value.Trim()));
         }
 
-        public void AddQueryString(string key, object value)
+        public virtual void AddQueryString(string key, object value)
         {
             // we check if the value is array, if so, then we have to break the array into multiple key value pairs
             bool isEnumerable = value?.GetType().IsArray ?? false;
@@ -48,6 +42,31 @@ namespace HttpClientExtended.Common
             {
                 AddKeyValuePair(key, ConvertValueToString(value));
             }
+        }
+
+        public virtual async Task<Uri> AsUriAsync(string baseUrl)
+        {
+            if (string.IsNullOrEmpty(baseUrl)) throw new ArgumentNullException("baseUrl");
+
+            UriKind uriKind = UriKind.RelativeOrAbsolute;
+
+            Uri uri = !this.Any()
+                ? new Uri(baseUrl, uriKind)
+                : new Uri($"{baseUrl}?{await AsUrlAsync()}",
+                    uriKind);
+
+            return uri;
+        }
+
+        public virtual async Task<string> AsUrlAsync()
+        {
+            string query;
+            using (var content = new FormUrlEncodedContent(this))
+            {
+                query = await content.ReadAsStringAsync();
+            }
+
+            return query;
         }
     }
 }
