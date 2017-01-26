98de9e0101cf05f6d9a4c7808441ab58506a822b
Zenon Trujillo <zenon.t@outlook.com>
2017-01-26 14:09:27 -0800

    unit test json/xml

--- HttpClientExtended/HttpClientExtensions.Abstractions.Test/HttpClientVerbBuilderTest.cs
+++ HttpClientExtended/HttpClientExtensions.Abstractions.Test/HttpClientVerbBuilderTest.cs
@@ -1,4 +1,5 @@
 ﻿using HttpClientExtended.Abstractions;
+using HttpClientExtended.Abstractions.Extensions;
 using HttpClientExtended.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -64,7 +65,69 @@ namespace HttpClientExtensions.Abstractions.Test
             // assert
             Assert.NotNull(queryBuilder.Content);
             Assert.Matches(queryBuilder.HttpMethod.Method, "POST");
-            var content = Assert.IsType<ObjectContent<FakePayload>>(queryBuilder.Content);
+            ObjectContent<FakePayload> content = Assert.IsType<ObjectContent<FakePayload>>(queryBuilder.Content);
+            var resultPayload = Assert.IsType<FakePayload>(content.Value);
+            Assert.NotNull(resultPayload);
+            Assert.True(resultPayload.Id == payload.Id);
+            Assert.True(resultPayload.NonNullDateTime == payload.NonNullDateTime);
+        }
+
+        [Fact]
+        public void ShouldBuildPostHttpMethodWithJsonPayload()
+        {
+            // arrange
+            const string url = "http://fakeurl/";
+            var payload = new FakePayload
+            {
+                Id = 10,
+                NonNullDateTime = new DateTime(2010, 1, 1, 10, 00, 00),
+                Note = "Note1",
+                NullableDateTime = new DateTime(2012, 1, 1, 1, 0, 0, 0),
+                Token = Guid.NewGuid()
+            };
+            var httpClient = new MockedHttpClient();
+            IHttpClientVerbBuilder<MockedHttpClient> builder = new HttpClientVerbBuilder<MockedHttpClient>(httpClient);
+
+            // act
+            IHttpClientQueryBuilder queryBuilder = builder.PostAsJson(url, payload);
+
+            // assert
+            Assert.NotNull(queryBuilder.Content);
+            Assert.Matches(queryBuilder.HttpMethod.Method, "POST");
+            ObjectContent content = Assert.IsType<ObjectContent>(queryBuilder.Content);
+            var resultPayload = Assert.IsType<FakePayload>(content.Value);
+            Assert.NotNull(resultPayload);
+            Assert.True(resultPayload.Id == payload.Id);
+            Assert.True(resultPayload.NonNullDateTime == payload.NonNullDateTime);
+        }
+
+        [Fact]
+        public void ShouldBuildPostHttpMethodWithXmlPayload()
+        {
+            // arrange
+            const string url = "http://fakeurl/";
+            var payload = new FakePayload
+            {
+                Id = 10,
+                NonNullDateTime = new DateTime(2010, 1, 1, 10, 00, 00),
+                Note = "Note1",
+                NullableDateTime = new DateTime(2012, 1, 1, 1, 0, 0, 0),
+                Token = Guid.NewGuid()
+            };
+            var httpClient = new MockedHttpClient();
+            IHttpClientVerbBuilder<MockedHttpClient> builder = new HttpClientVerbBuilder<MockedHttpClient>(httpClient);
+
+            // act
+            IHttpClientQueryBuilder queryBuilder = builder.PostAsXml(url, payload);
+
+            // assert
+            Assert.NotNull(queryBuilder.Content);
+            Assert.Matches(queryBuilder.HttpMethod.Method, "POST");
+            ObjectContent content = Assert.IsType<ObjectContent>(queryBuilder.Content);
+            var resultPayload = Assert.IsType<FakePayload>(content.Value);
+            Assert.NotNull(resultPayload);
+            Assert.True(resultPayload.Id == payload.Id);
+            Assert.True(resultPayload.NonNullDateTime == payload.NonNullDateTime);
         }
     }
 }
