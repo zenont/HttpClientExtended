658a8c0b7e0e502a4315a1c8a014b9fbed0cf4cc
Zenon Trujillo <zenon.t@outlook.com>
2017-01-26 14:25:59 -0800

    building fake web api for testing

---
+++ HttpClientExtended/HttpClientExtensions.Abstractions.Test/Controllers/FakeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientExtensions.Abstractions.Test.Controllers
+{
+    [Route("api/test")]
+    public class FakeController:Controller
+    {
+
+        [HttpGet("")]
+        public IActionResult Get([FromQuery] int? id)
+        {
+            return Ok(new FakePayload
+            {
+                Id = id ?? -1
+            });
+        }
+    }
+}
---
+++ HttpClientExtended/HttpClientExtensions.Abstractions.Test/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace HttpClientExtensions.Abstractions.Test
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+
+        }
+
+        public void Configuration(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseMvc();
+        }
+    }
+}
